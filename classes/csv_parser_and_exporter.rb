032dd4750094c6b2ab64ce41c1f6f5129ae9530e
Joe <pickertjoe@gmail.com>
2021-07-15 17:05:16 -0500

    Creating stubs for classes and specs

---
+++ classes/csv_parser_and_exporter.rb
@@ -0,0 +1,11 @@
+class CSVParserAndExporter
+  def initialize(reader: , parser: , writer:)
+    @reader = reader
+    @parser = parser
+    @writer = writer
+  end
+
+  def run
+    
+  end
+end
---
+++ classes/data_formatter.rb
@@ -0,0 +1,3 @@
+class DataFormatter
+
+end
