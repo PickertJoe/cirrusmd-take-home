e3719e7e9b9d8c744eecfc6617715c21e3987174
Joe <pickertjoe@gmail.com>
2021-07-15 17:13:35 -0500

    Define expectations for the class specs

---
+++ spec/support/file_reader_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../classes/csv_reader'
+
+describe CSVReader do
+  let(:csv_reader) { CSVReader.new }
+  let(:expected_raw_csv_data) {}
+
+  it "should read the raw data from input.csv" do
+    pending
+  end
+end
---
+++ spec/support/report_generator_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../classes/report_generator.rb"
+
+describe ReportGenerator do
+  let (:report_generator) { ReportGenerator.new }
+
+  it "should export the formatted data to output.csv" do
+    pending
+  end
+
+  it "should export the invalid data to report.txt" do
+    pending
+  end
+
+  it "should export the number of invalid records to report.txt" do
+    pending
+  end
+
+  it "should export the number of valid records to report.txt" do
+    pending
+  end
+end
