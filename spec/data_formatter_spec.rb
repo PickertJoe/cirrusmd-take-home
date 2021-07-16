35737b9367d7647186b3761ebcadc823deb22da1
Joe <pickertjoe@gmail.com>
2021-07-15 19:59:09 -0500

    Test coverage for presence validation and iso8601 date formatting

---
+++ spec/data_formatter_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../classes/data_formatter'
+
+describe DataFormatter do
+  let(:data_formatter) { DataFormatter.new }
+  let(:formatter_input) { File.read(File.join(File.dirname(__FILE__), './support/test_input.csv')) }
+
+  it "should mark rows that are missing a required field as invalid" do
+    data_formatter.parse(input: formatter_input)
+    expect(data_formatter.invalid_data.length).to eq 1
+  end
+
+  it "should return dates in the proper iso8601 format" do
+    data_formatter.parse(input: formatter_input)
+    expect(data_formatter.valid_data.map{ |row| row[:dob] }).to all(match(/\d{4}-\d{2}-\d{2}/))
+    expect(data_formatter.valid_data.map{ |row| row[:effective_date] }).to all(match(/\d{4}-\d{2}-\d{2}/))
+    expect(data_formatter.valid_data.map{ |row| row[:expiry_date] }).to all(match(/\d{4}-\d{2}-\d{2}/))
+  end
+
+  it "should return phone numbers in the proper E.164 format" do
+    pending
+  end
+
+  it "should mark rows whose phone numbers cannot be formatted as invalid" do
+    pending
+  end
+end
---
+++ spec/file_reader_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../classes/csv_reader'
+
+describe CSVReader do
+  let(:csv_reader) { CSVReader.new }
+  let(:expected_raw_csv_data) { "first_name,last_name,dob,member_id,effective_date,expiry_date,phone_number\nJason,Bateman ,12/12/2010,AB 0000,,,\nBrent ,Wilson,1/1/1988,349090,9/30/19,9/30/2000,(303) 887 3456\nAntonio,Brown ,2/2/1966,890887,9/30/2000,303-333-9987\n" }
+
+  it "should read the raw data from input.csv" do
+    expect(csv_reader.read(file_name: "../spec/support/test_input.csv")).to eq(expected_raw_csv_data)
+  end
+end
